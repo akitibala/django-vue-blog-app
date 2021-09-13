67d62b84824db10e17f60d9c17fc7349205cad72
Reddy <akiti.bala.murali.krishna.reddy@hp.com>
2021-09-13 16:54:42 +0530

    Rest-API Created for post

---
+++ backend/views.py
@@ -0,0 +1,29 @@
+# from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics
+from backend import serializers
+from django.contrib.auth.models import User
+from backend.models import Post
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
