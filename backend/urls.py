6fda9bdbaef4e4024f0634f6452a05d3c28361a0
Reddy <akiti.bala.murali.krishna.reddy@hp.com>
2021-09-14 16:21:11 +0530

    permissions added

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from backend import views
 
 urlpatterns = [
+    # path('users/', views.UserList.as_view()),
+    # path('users/<int:pk>/', views.UserDetail.as_view()),
     path('posts/', views.PostList.as_view()),
     path('posts/<int:pk>/', views.PostDetail.as_view()),
     path('posts', views.SearchList.as_view()),
